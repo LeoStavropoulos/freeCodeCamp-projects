dd4cac9448c55ca550c750207159ee352fbbc979
Leonardos <stavropoulos.leonardos@gmail.com>
2022-07-25 02:01:08 +0300

    fixed hidden class bug

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 const tabs = document.querySelectorAll(".nav-links>li");
-
 tabs.forEach(tab => tab.addEventListener("click", e => switchTab(e)));
 
 function switchTab(e) {
     const id = e.target.id
     document.querySelector(".selected").classList.remove("selected");
     e.target.classList.add("selected");
-    document.querySelector(".project-section").classList.add("hidden");
-    document.querySelector(`#${id}Section`).classList.remove("hidden")
-
+    document.querySelector(".project-section:not(.hidden)").classList.add("hidden");
+    document.querySelector(`#${id}Section`).classList.remove("hidden");
 }
