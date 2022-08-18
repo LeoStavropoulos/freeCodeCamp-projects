05ac44dfadf63727ad4f10da7c50ee9c212c5377
Leonardos <stavropoulos.leonardos@gmail.com>
2022-08-18 23:01:16 +0300

    Created drum machine's basic functionality

--- front-end-development-libraries/front-end-development-libraries/src/pages/build-a-drum-machine/DrumMachine.js
+++ front-end-development-libraries/front-end-development-libraries/src/pages/build-a-drum-machine/DrumMachine.js
@@ -1,14 +1,50 @@
 import "./DrumMachine.css"
 import {faFreeCodeCamp} from "@fortawesome/free-brands-svg-icons/faFreeCodeCamp";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {useEffect, useState} from "react";
+import Switch from "./components/Switch";
+import Display from "./components/Display";
+import Slider from "./components/Slider";
+import DrumPad from "./components/DrumPad";
+import bankOne from "./banks/bankOne";
 
 const DrumMachine = () => {
+    const [display, setDisplay] = useState("");
+    const [soundBank, setSoundBank] = useState(bankOne);
+    const [powerOn, setPowerOn] = useState(true);
+    const [volume, setVolume] = useState(30);
+
+    useEffect(e => {
+        document.addEventListener("keydown", e => playSound(e.key.toUpperCase()))
+    }, []);
+
+    const playSound = selector => {
+        const audio = document.getElementById(selector).firstElementChild;
+        audio.play();
+        soundBank.forEach(element => {
+            if (element.keyTrigger === selector) {
+                setDisplay(element.id);
+            }
+        })
+    }
+
     return (
         <div className="drum-machine-container">
-            <div className="drum-machine">
+            <div id="drum-machine" className="drum-machine">
                 <header>
                     FCC <FontAwesomeIcon icon={faFreeCodeCamp}/>
                 </header>
+                <div className="drum-machine-inner-container">
+                    <div className="drum-pad-grid">
+                        {soundBank.map(pad => <DrumPad key={pad.keyCode} text={pad.keyTrigger} audio={pad.url} setDisplay={setDisplay} name={pad.id} playSound={playSound}/>)}
+                    </div>
+                    <div className="controls">
+                        <Switch name="Power"/>
+                        <Display text={display}/>
+                        <Slider />
+                        <Switch name="Bank"/>
+                    </div>
+                </div>
             </div>
         </div>
     )
--- front-end-development-libraries/front-end-development-libraries/src/pages/build-a-drum-machine/components/DrumPad.js
+++ front-end-development-libraries/front-end-development-libraries/src/pages/build-a-drum-machine/components/DrumPad.js
@@ -1,5 +1,13 @@
+import {useEffect} from "react";
+
 const DrumPad = (props) => {
 
+    return(
+        <button className="drum-pad" id={props.text} onClick={() => props.playSound(props.text)}>
+            {props.text}
+            <audio className="clip" id={props.text} src={props.audio}></audio>
+        </button>
+    )
 }
 
 export default DrumPad;
